bb81124f356df060cedb6aac78c5946c55d6ade4
JasonGITHUB <Jasonvirtualai@gmail.com>
2019-10-13 16:54:10 -0700

    add  copy right

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Us',
-        name: 'Axson Engineering'
+        name: 'Axson Engineering, Copy right Jason'
     })
 })
 
